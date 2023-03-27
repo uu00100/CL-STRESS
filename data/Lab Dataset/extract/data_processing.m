0edab384dd3741c45e5869c7ee16f23285fcfdac
Ugo <uu00100@georgiasouthern.edu>
2023-03-26 22:33:39 -0400

    ugo

--- data/Lab Dataset/extract/data_processing.m
+++ data/Lab Dataset/extract/data_processing.m
@@ -3,12 +3,12 @@ clear;
 clc;
 
 %% Folders to process
-train = ["DA1_WC", "DA2_NS", "DA3_CP", "DA4_JC", "DA7_RF", "SA9_CP"];
-test = ["DA8_EP"];
-id_train = [0 1 2 3 4 2];
-id_test = [5];
-state_train = [1 1 1 1 1 2];
-state_test = [1];
+train = ["DA1_WC", "DA2_AS", "DA3_CP", "DA4_JC", "DA5_UU", "DA6_AR", "DA9_NR", "SA10_CP", "SA11_UU", "SA12_AS", "SA13_WC", "SA14_AR", "SA15_NR", "SA16_JC"];
+test = ["DA8_EP", "SA17_EP"];
+id_train = [0 1 2 3 4 5 6 2 4 1 0 5 6 3];
+id_test = [7 7];
+state_train = [1 1 1 1 1 1 1 2 2 2 2 2 2 2];
+state_test = [1 2];
 
 %% Train
 extraction(train, "train");
@@ -19,6 +19,3 @@ combining_files(train, "train");
 extraction(test, "test");
 transform(test, id_test, state_test);
 combining_files(test, "test");
-
-%% Normalize
-normalize
--- data/Lab Dataset/extract/normalize_v2.m
+++ data/Lab Dataset/extract/normalize_v2.m
@@ -2,8 +2,8 @@ function hold = normalize_v2(hold)
   	maxA = max(hold);
  	minA = min(hold);
 	
-  	m = (1 - (-1)) / (maxA - minA);
-  	c = 1 - (m * maxA);
+  	m = (1 - (-1)) ./ (maxA - minA);
+  	c = 1 - (m .* maxA);
  	
  	hold = (m .* hold) + c;
 end
